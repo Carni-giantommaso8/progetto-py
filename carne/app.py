662e2669e54ba09ef7869aa2c53383d7b726ee27
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-04-04 08:59:49 +0000

    spiegazione

--- carne/app.py
+++ carne/app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista = ["luca","pizza","motore","focaccia","telefono"]
+
 @app.route('/')
+def aggiungi():
+    elemento = re
+
+
 def home():
-    return render_template("index.html")
+
+    return render_template("index.html" , lista = lista) 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
