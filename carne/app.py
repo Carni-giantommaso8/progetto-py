0fa74f7428d53c68bc825b5b8a647ffee97addae
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-04-04 10:37:00 +0000

    progetto con rimuovi e aggiungi alla lista

--- carne/app.py
+++ carne/app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
 lista = ["luca","pizza","motore","focaccia","telefono"]
 
 @app.route('/')
-def aggiungi():
-    elemento = re
-
-
 def home():
 
     return render_template("index.html" , lista = lista) 
 
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista):
+        lista.pop(indice)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
